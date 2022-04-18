318e4364cbe5e5783be24fc8c32f81d10941efaf
Smarsden007 <seannmarsden@gmail.com>
2022-04-17 17:19:58 -0700

    working on class comps

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,20 +1,75 @@
-import React from 'react'
+import React from "react";
 
 export default class AppClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 2,
+      y: 2,
+      numberOfSteps: 0,
+      email: "",
+      currentIndex: 4,
+    };
+  }
+
+  handleInput = (e) => {
+    e.preventDefault();
+    this.setState({ ...this.state, email: [e.target.value] });
+  };
+
+  handleDirectionalInput = (e) => {
+    e.preventDefault();
+    switch (e.target.id) {
+      case "up":
+        if (this.state.y > 1) {
+          this.setState({ ...this.state, y: this.state.y - 1, numberOfSteps: this.state.numberOfSteps+1 });
+          document.getElementById("grid").children[this.state.currentIndex].classList = 'square'
+          document.getElementById("grid").children[this.state.currentIndex -3].classList = 'square active'
+        }
+        break;
+      case "down":
+        if (this.state.y < 3) {
+          this.setState({ ...this.state, y: this.state.y + 1, numberOfSteps: this.state.numberOfSteps+1});
+        }
+        break;
+      case "left":
+        if (this.state.x > 1) {
+          this.setState({ ...this.state, x: this.state.x - 1, numberOfSteps: this.state.numberOfSteps+1 });
+        }
+        break;
+      case "right":
+        if (this.state.x < 3) {
+          this.setState({ ...this.state, x: this.state.x + 1, numberOfSteps: this.state.numberOfSteps+1 });
+        }
+        break;
+      default:
+        break;
+    }
+    console.log("##########", this.state.x, this.state.y);
+  };
+  
+  handleReset = (e)=>{
+    this.setState({
+      x: 2,
+      y: 2,
+      numberOfSteps: 0,
+      email: "",
+    })
+  }
   render() {
-    const { className } = this.props
+    const { className } = this.props;
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">{`Coordinates (${this.state.x}, ${this.state.y})`}</h3>
+          <h3 id="steps">{`You moved ${this.state.numberOfSteps} times`}</h3>
         </div>
         <div id="grid">
           <div className="square"></div>
           <div className="square"></div>
           <div className="square"></div>
           <div className="square"></div>
-          <div className="square active">B</div>
+          <div className="square active"></div>
           <div className="square"></div>
           <div className="square"></div>
           <div className="square"></div>
@@ -24,17 +79,33 @@ export default class AppClass extends React.Component {
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={this.handleDirectionalInput} id="left">
+            LEFT
+          </button>
+          <button onClick={this.handleDirectionalInput} id="up">
+            UP
+          </button>
+          <button onClick={this.handleDirectionalInput} id="right">
+            RIGHT
+          </button>
+          <button onClick={this.handleDirectionalInput} id="down">
+            DOWN
+          </button>
+          <button onClick={this.handleReset} id="reset">
+            reset
+          </button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email"></input>
+          <input
+            id="email"
+            type="email"
+            placeholder="type email"
+            value={this.state.email}
+            onChange={this.handleInput}
+          ></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
-    )
+    );
   }
 }
